00b064c5c26b62910481256c6533a68a866ae630
Ran Nachmany <Ran@mobiliup.com>
2013-03-02 23:12:28 +0200

    Implementing single lecture as activity with fragment

--- src/com/gdg/andconlab/ui/MainActivity.java
+++ src/com/gdg/andconlab/ui/MainActivity.java
@@ -5,19 +5,10 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
-import android.widget.ListView;
 
-import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.app.SherlockFragmentActivity;
 import com.gdg.andconlab.CommunicationService;
-import com.gdg.andconlab.DBUtils;
-import com.gdg.andconlab.DatabaseHelper;
 import com.gdg.andconlab.R;
 
 /**
@@ -27,33 +18,32 @@ import com.gdg.andconlab.R;
  * @author Ran Nachmany
  *
  */
-public class MainActivity extends SherlockActivity implements OnItemClickListener{
+public class MainActivity extends SherlockFragmentActivity implements LecturesListFragment.callbacks{
 
-	private ListView mList;
 	private ProgressDialog mProgressDialog;
 	private BroadcastReceiver mUpdateReceiver;
+	private LecturesListFragment mLecturesFragment;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
 		setContentView(R.layout.main_activity);
-		mList = (ListView) findViewById(R.id.list);
-
-		mList.setOnItemClickListener(this);
 		
 		mUpdateReceiver = new BroadcastReceiver() {
 			//TODO: [Ran] handle network failure
 			@Override
 			public void onReceive(Context context, Intent intent) {
 				if (intent.getAction().equalsIgnoreCase(CommunicationService.RESULTS_ARE_IN)) {
-					new lecturesLoader().execute((Void) null);
+					mLecturesFragment.reloadLecturesFromDb();
 				}
 
 				if (null != mProgressDialog)
 					mProgressDialog.dismiss();
 			}
 		};
+		
+		mLecturesFragment = (LecturesListFragment) getSupportFragmentManager().findFragmentById(R.id.lectures_fragment);
 	}
 
 	@Override
@@ -72,55 +62,23 @@ public class MainActivity extends SherlockActivity implements OnItemClickListene
 		filter.addAction(CommunicationService.RESULTS_ARE_IN);
 		registerReceiver(mUpdateReceiver, filter);
 		
-		new lecturesLoader().execute((Void)null);
+	}
+	
+	////////////////////////////
+	// Fragment interface
+	///////////////////////////
+	@Override
+	public void onLectureClicked(long lectureId) {
+		Intent i = new Intent(this,SingleLectureActivity.class);
+		i.putExtra(SingleLectureActivity.EXTRA_LECTURE_ID, lectureId);
+		startActivity(i);
 	}
 
 	@Override
-	public void onItemClick(AdapterView<?> list, View view, int position, long id) {
-		Intent i = new Intent(this,SingleLectureActivity.class);
-		i.putExtra(SingleLectureActivity.EXTRA_LECTURE_ID, id);
-		startActivity(i);
-	}
-	
-	
-	private void refreshList(boolean firstLoad) {
-		if (firstLoad) {
-			mProgressDialog = ProgressDialog.show(this, getString(R.string.progress_dialog_starting_title), getString(R.string.progress_dialog_starting_message));
-		}
-		
+	public void fetchLecturesFromServer() {
 		Intent i = new Intent(this,CommunicationService.class);
 		startService(i);
-//		ServerCommunicationManager.getInstance(getApplicationContext()).startSearch("Android", 1);
+		mProgressDialog = ProgressDialog.show(this, getString(R.string.progress_dialog_starting_title), getString(R.string.progress_dialog_starting_message));
 	}
 
-	////////////////////////////////
-	// Async task that queries the DB in background
-	////////////////////////////////
-	private class lecturesLoader extends AsyncTask<Void, Void, Cursor> {
-		@Override
-		protected Cursor doInBackground(Void... params) {
-
-
-			SQLiteDatabase db = new DatabaseHelper(MainActivity.this.getApplicationContext(), DatabaseHelper.DB_NAME,null , DatabaseHelper.DB_VERSION).getReadableDatabase();
-			return DBUtils.getEventsCurosr(db);
-		}
-
-		@Override
-		protected void onPostExecute(Cursor result) {
-			if (0 == result.getCount()) {
-				//we don't have anythign in our DB, force network refresh
-				refreshList(true);
-			}
-			else {
-				LecturesAdapter adapter = (LecturesAdapter) mList.getAdapter();
-				if (null == adapter) {
-					adapter = new LecturesAdapter(MainActivity.this.getApplicationContext(), result);
-					mList.setAdapter(adapter);	
-				}
-				else {
-					adapter.changeCursor(result);
-				}
-			}
-		}
-	}
 }
--- src/com/gdg/andconlab/ui/SingleLectureActivity.java
+++ src/com/gdg/andconlab/ui/SingleLectureActivity.java
@@ -14,6 +14,7 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.app.SherlockFragmentActivity;
 import com.actionbarsherlock.view.MenuItem;
 import com.gdg.andconlab.DBUtils;
 import com.gdg.andconlab.DatabaseHelper;
@@ -28,11 +29,13 @@ import com.pagesuite.flowtext.FlowTextView;
  * @author Ran Nachmany
  *
  */
-public class SingleLectureActivity extends SherlockActivity{
+public class SingleLectureActivity extends SherlockFragmentActivity{
 
 	public static final String EXTRA_LECTURE_ID = "lecture_id";
 	
 	private static final int ILLEGAL_ID = -1;
+	
+	private SingleLectureFragment mFragment;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -41,39 +44,15 @@ public class SingleLectureActivity extends SherlockActivity{
 		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 		getSupportActionBar().setHomeButtonEnabled(true);
 		
-		setContentView(R.layout.single_lecture);
+		setContentView(R.layout.single_lecture_activity);
 		
 		long id = getIntent().getLongExtra(EXTRA_LECTURE_ID, ILLEGAL_ID);
 		if (ILLEGAL_ID == id) {
 			throw new IllegalStateException("You should set lecture ID in the calling intent");
 		}
 		else {
-			//fetch the lecture from db
-			SQLiteDatabase db = new DatabaseHelper(getApplicationContext(), DatabaseHelper.DB_NAME, null, DatabaseHelper.DB_VERSION).getReadableDatabase();
-			Lecture lecture = DBUtils.getLectureById(db, id);
-			
-			//set lecture name and description
-			((TextView)findViewById(R.id.lecture_name)).setText(lecture.getName());
-			((TextView)findViewById(R.id.lecture_description)).setText(lecture.getDescription());
-			
-			//get all speakers
-			ArrayList<Speaker> speakers = DBUtils.getSpeakersByLectureId(db, id);
-			
-			//loop and add speakers.
-			LinearLayout ll = (LinearLayout) findViewById(R.id.lecture_container);
-			FlowTextView ftv;
-			ImageView img;
-			for (Speaker speaker : speakers) {
-				ftv = new FlowTextView(this);
-				img = new ImageView(this);
-				img.setImageResource(R.drawable.andconlablauncher);
-				Spanned span = Html.fromHtml("<b>"+speaker.getFirstName() + " " + speaker.getLastName() +"</B><BR>" + speaker.getBio());
-				ftv.addView(img);
-				ftv.setText(span);
-				ftv.setTextSize(getResources().getDimensionPixelSize(R.dimen.sub_title_size));
-				ftv.invalidate();
-				ll.addView(ftv);
-			}
+			mFragment = (SingleLectureFragment) getSupportFragmentManager().findFragmentById(R.id.single_lecture_fragment);
+			mFragment.setLectureId(id);
 		}
 	}
 	
