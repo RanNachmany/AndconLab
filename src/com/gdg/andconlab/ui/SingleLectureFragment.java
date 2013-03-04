4826e5d92c978304a85dc5a0e6b000a3d05d0087
Ran Nachmany <Ran@mobiliup.com>
2013-03-04 13:04:31 +0200

    fixed layout issue

--- src/com/gdg/andconlab/ui/SingleLectureFragment.java
+++ src/com/gdg/andconlab/ui/SingleLectureFragment.java
@@ -31,110 +31,115 @@ import java.util.ArrayList;
 
 public class SingleLectureFragment extends SherlockFragment implements OnClickListener {
 
-    private static final String TAG = "SingleLectureFragment";
-    public static final String LECTURE_ID = "lecture_id";
-    private static final int ILLEGAL_ID = -1;
+	private static final String TAG = "SingleLectureFragment";
+	public static final String LECTURE_ID = "lecture_id";
+	private static final int ILLEGAL_ID = -1;
 
 
-    private View mRootView;
-    private Lecture mLecture;
+	private View mRootView;
+	private Lecture mLecture;
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        mRootView = inflater.inflate(R.layout.single_lecture_fragment, null);
-        if (null != getArguments()) {
-        	setLectureId(getArguments().getLong(LECTURE_ID));
-        }
-        return mRootView;
-    }
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		mRootView = inflater.inflate(R.layout.single_lecture_fragment, null);
+		if (null != getArguments()) {
+			setLectureId(getArguments().getLong(LECTURE_ID));
+		}
+		return mRootView;
+	}
 
-    public void setLectureId(long id) {
-        //fetch the lecture from db
-        SQLiteDatabase db = new DatabaseHelper(getActivity().getApplicationContext(), DatabaseHelper.DB_NAME, null, DatabaseHelper.DB_VERSION).getReadableDatabase();
-        Lecture lecture = DBUtils.getLectureById(db, id);
+	public void setLectureId(long id) {
+		//fetch the lecture from db
+		SQLiteDatabase db = new DatabaseHelper(getActivity().getApplicationContext(), DatabaseHelper.DB_NAME, null, DatabaseHelper.DB_VERSION).getReadableDatabase();
+		Lecture lecture = DBUtils.getLectureById(db, id);
 
-        //set lecture details
-        getSherlockActivity().getSupportActionBar().setTitle(lecture.getName());
-        String description = lecture.getDescription();
-        if (description != null && !TextUtils.isEmpty(description.trim())) {
-            ((TextView) mRootView.findViewById(R.id.lecture_description)).setText(description);
-        } else {
-            mRootView.findViewById(R.id.lecture_description).setVisibility(View.GONE);
-        }
+		//set lecture details
+		getSherlockActivity().getSupportActionBar().setTitle(lecture.getName());
+		String description = lecture.getDescription();
+		if (description != null && !TextUtils.isEmpty(description.trim())) {
+			((TextView) mRootView.findViewById(R.id.lecture_description)).setText(description);
+		}
+//		else {
+//			mRootView.findViewById(R.id.lecture_description).setVisibility(View.GONE);
+//		}
 
-        String slidesUrl = lecture.getSlidesUrl();
-        String videoUrl = lecture.getVideoUrl();
-        boolean showButtons = false;
-        if (!TextUtils.isEmpty(slidesUrl)) {
-            View btnSlides = mRootView.findViewById(R.id.lecture_btn_slides);
-            btnSlides.setVisibility(View.VISIBLE);
-            btnSlides.setOnClickListener(this);
-            showButtons = true;
-        }
+		String slidesUrl = lecture.getSlidesUrl();
+		String videoUrl = lecture.getVideoUrl();
+		boolean showButtons = false;
+		if (!TextUtils.isEmpty(slidesUrl)) {
+			View btnSlides = mRootView.findViewById(R.id.lecture_btn_slides);
+			if (null != btnSlides) {
+				btnSlides.setVisibility(View.VISIBLE);
+				btnSlides.setOnClickListener(this);
+				showButtons = true;
+			}
+		}
 
-        if (!TextUtils.isEmpty(videoUrl)) {
-            View btnVideo = mRootView.findViewById(R.id.lecture_btn_video);
-            btnVideo.setVisibility(View.VISIBLE);
-            btnVideo.setOnClickListener(this);
-            showButtons = true;
-        }
+		if (!TextUtils.isEmpty(videoUrl)) {
+			View btnVideo = mRootView.findViewById(R.id.lecture_btn_video);
+			if (null != btnVideo) {
+				btnVideo.setVisibility(View.VISIBLE);
+				btnVideo.setOnClickListener(this);
+				showButtons = true;
+			}
+		}
 
-        if (showButtons) {
-            mRootView.findViewById(R.id.lecture_btns_wrapper).setVisibility(View.VISIBLE);
-        }
+		if (showButtons) {
+			mRootView.findViewById(R.id.lecture_btns_wrapper).setVisibility(View.VISIBLE);
+		}
 
-        //get all speakers
-        ArrayList<Speaker> speakers = DBUtils.getSpeakersByLectureId(db, id);
+		//get all speakers
+		ArrayList<Speaker> speakers = DBUtils.getSpeakersByLectureId(db, id);
 
-        //loop and add speakers.
-        LinearLayout ll = (LinearLayout) mRootView.findViewById(R.id.lecture_container);
-        int speakerTxtSize = getResources().getDimensionPixelSize(R.dimen.sub_title_size);
-        final LayoutInflater layoutInflater = getActivity().getLayoutInflater();
-        for (Speaker speaker : speakers) {
-            FlowTextView ftv = (FlowTextView) layoutInflater.inflate(layout.speaker_item, ll, false);
-            ImageView img = (ImageView) ftv.findViewById(R.id.speaker_item_img);
+		//loop and add speakers.
+		LinearLayout ll = (LinearLayout) mRootView.findViewById(R.id.lecture_container);
+		int speakerTxtSize = getResources().getDimensionPixelSize(R.dimen.sub_title_size);
+		final LayoutInflater layoutInflater = getActivity().getLayoutInflater();
+		for (Speaker speaker : speakers) {
+			FlowTextView ftv = (FlowTextView) layoutInflater.inflate(layout.speaker_item, ll, false);
+			ImageView img = (ImageView) ftv.findViewById(R.id.speaker_item_img);
 
-            ImageManager.loadImage(getActivity(), ImageType.SPEAKER, speaker.getImageUrl(), img);
-            Spanned span = Html.fromHtml("<b>" + speaker.getFullName() + "</b><br/>" + speaker.getBio());
-            ftv.setText(span);
-            ftv.setTextSize(speakerTxtSize);
-            ftv.invalidate();
-            ll.addView(ftv);
-        }
+			ImageManager.loadImage(getActivity(), ImageType.SPEAKER, speaker.getImageUrl(), img);
+			Spanned span = Html.fromHtml("<b>" + speaker.getFullName() + "</b><br/>" + speaker.getBio());
+			ftv.setText(span);
+			ftv.setTextSize(speakerTxtSize);
+			ftv.invalidate();
+			ll.addView(ftv);
+		}
 
-        db.close();
-        mLecture = lecture;
-    }
+		db.close();
+		mLecture = lecture;
+	}
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case id.lecture_btn_slides:
-                openWebsite(mLecture.getSlidesUrl());
-                break;
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case id.lecture_btn_slides:
+			openWebsite(mLecture.getSlidesUrl());
+			break;
 
-            case id.lecture_btn_video:
-                openWebsite(mLecture.getVideoUrl());
-                break;
-        }
-    }
+		case id.lecture_btn_video:
+			openWebsite(mLecture.getVideoUrl());
+			break;
+		}
+	}
 
 
-    //////////////////////////////////////////
-    // Private
-    //////////////////////////////////////////
+	//////////////////////////////////////////
+	// Private
+	//////////////////////////////////////////
 
-    /**
-     * Request to open the browser on given <code>url</code>
-     *
-     * @param url
-     */
-    private void openWebsite(String url) {
-        try {
-            BrowserUtils.openBrowserForUrl(getActivity(), url);
-        } catch (URISyntaxException e) {
-            SLog.e(TAG, e, "Couldn't open website: [%s]", url);
-        }
-    }
+	/**
+	 * Request to open the browser on given <code>url</code>
+	 *
+	 * @param url
+	 */
+	private void openWebsite(String url) {
+		try {
+			BrowserUtils.openBrowserForUrl(getActivity(), url);
+		} catch (URISyntaxException e) {
+			SLog.e(TAG, e, "Couldn't open website: [%s]", url);
+		}
+	}
 
 }
